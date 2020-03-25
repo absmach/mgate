fe281a08e76b6325f01177a4b804633be1f7db92
Nikola Marcetic <n.marcetic86@gmail.com>
2020-03-25 12:59:36 +0100

    Clean vendor
    Signed-off-by: Nikola Marcetic <n.marcetic86@gmail.com>

--- vendor/github.com/mainflux/mainflux/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Mainflux
-// SPDX-License-Identifier: Apache-2.0
-
-// Package mainflux acts as an umbrella package containing multiple different
-// microservices and defines all shared domain concepts.
-package mainflux
