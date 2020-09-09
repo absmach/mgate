c569da23d52908a1a5b84f86a1dbe972c3152bc3
Ivan Milošević <iva@blokovi.com>
2020-09-09 23:01:51 +0200

    remove mqttv3.1 from dialer subprotocol list
    Signed-off-by: Ivan Milošević <iva@blokovi.com>

--- pkg/websocket/websocket.go
+++ pkg/websocket/websocket.go
@@ -34,7 +34,7 @@ var upgrader = websocket.Upgrader{
 	// Timeout for WS upgrade request handshake
 	HandshakeTimeout: 10 * time.Second,
 	// Paho JS client expecting header Sec-WebSocket-Protocol:mqtt in Upgrade response during handshake.
-	Subprotocols: []string{"mqttv3.1"},
+	Subprotocols: []string{"mqttv3.1", "mqtt"},
 	// Allow CORS
 	CheckOrigin: func(r *http.Request) bool {
 		return true
@@ -69,7 +69,7 @@ func (p Proxy) pass(in *websocket.Conn) {
 	}
 
 	dialer := &websocket.Dialer{
-		Subprotocols: []string{"mqttv3.1"},
+		Subprotocols: []string{"mqtt"},
 	}
 	srv, _, err := dialer.Dial(url.String(), nil)
 
