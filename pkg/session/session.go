072a3dffbb3e438309a7bd8f7c717951a4c26ce4
Ivan Milošević <iva@blokovi.com>
2020-09-17 23:40:58 +0200

    remove debug print
    Signed-off-by: Ivan Milošević <iva@blokovi.com>

--- pkg/session/session.go
+++ pkg/session/session.go
@@ -3,7 +3,6 @@ package session
 import (
 	"crypto/tls"
 	"crypto/x509"
-	"fmt"
 	"net"
 
 	"github.com/eclipse/paho.mqtt.golang/packets"
@@ -159,7 +158,6 @@ func clientCert(conn net.Conn) (x509.Certificate, error) {
 		cert := *state.PeerCertificates[0]
 		return cert, nil
 	default:
-		fmt.Printf("I don't know about type %T!\n", connVal)
 		return x509.Certificate{}, nil
 	}
 }
